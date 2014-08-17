92aed981133bcbb6605c8c68fb22a17848737c51
Alessio Parma <alessio.parma@gmail.com>
2014-08-17 17:41:15 +0200

    Initial import

---
+++ ObjectPool/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CodeProject.ObjectPool")]
+[assembly: AssemblyDescription("An implementation of a Generic Object Pool.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ofir Makmal")]
+[assembly: AssemblyProduct("Generic Object Pool")]
+[assembly: AssemblyCopyright("Copyright © Ofir Makmal 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c5f877f9-c262-4346-bade-d6ad47e45de1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
