da7f1a5a0c626d9567116e192e241adbefe696ca
Alessio Parma <alessio.parma@gmail.com>
2014-08-18 11:32:00 +0200

    bump to 1.0.4

--- ObjectPool/Properties/AssemblyInfo.cs
+++ ObjectPool/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -31,5 +33,12 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyVersion("1.0.4")]
+[assembly: AssemblyFileVersion("1.0.4")]
+
+// Common Language Specification (CLS) compliance generally refers to the claim 
+// that CLS rules and restrictions are being followed.
+[assembly: CLSCompliant(true)]
+
+// Specifies that an assembly cannot cause an elevation of privilege.
+[assembly: SecurityTransparent]
