f2f4086a208131fda0ea98c1e20d4fd83806a2b5
Alessio Parma <alessio.parma@gmail.com>
2015-01-06 10:25:34 +0100

    Merge with dev-pomma89

--- ObjectPool/Properties/AssemblyInfo.cs
+++ ObjectPool/Properties/AssemblyInfo.cs
@@ -20,8 +20,8 @@ using System.Security;
 // 
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.1")]
+[assembly: AssemblyFileVersion("1.3.1")]
 
 // Common Language Specification (CLS) compliance generally refers to the claim that CLS rules and
 // restrictions are being followed.
