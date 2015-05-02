bc6def5e46f2d9b2897b17a0e42fdd64f788d4d3
Alessio Parma <alessio.parma@gmail.com>
2015-05-02 14:08:33 +0200

    Merge with default

---
+++ ObjectPool/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Generic Object Pool Implementation
+ *
+ * Implemented by Ofir Makmal, 28/1/2013
+ *
+ * My Blog: Blogs.microsoft.co.il/blogs/OfirMakmal
+ * Email:   Ofir.Makmal@gmail.com
+ *
+ */
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// General Information about an assembly is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with an assembly.
+[assembly: AssemblyTitle("CodeProject.ObjectPool")]
+[assembly: AssemblyDescription("An implementation of a Generic Object Pool.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ofir Makmal")]
+[assembly: AssemblyProduct("Generic Object Pool")]
+[assembly: AssemblyCopyright("Copyright © Ofir Makmal 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+// 
+// Major Version Minor Version Build Number Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers by using the '*'
+// as shown below: [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.4.2")]
+[assembly: AssemblyFileVersion("1.4.2")]
+
+// Common Language Specification (CLS) compliance generally refers to the claim that CLS rules and
+// restrictions are being followed.
+[assembly: CLSCompliant(true)]
+
+// To allow simpler unit testing.
+[assembly: InternalsVisibleTo("UnitTests")]
