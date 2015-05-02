4b777b792199643ed64e93edb9322bd1c60c7162
Alessio Parma <alessio.parma@gmail.com>
2015-05-02 15:30:11 +0200

    v1.5.0

--- ObjectPool/Properties/AssemblyInfo.cs
+++ ObjectPool/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.CompilerServices;
 // 
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.2")]
-[assembly: AssemblyFileVersion("1.4.2")]
+[assembly: AssemblyVersion("1.5.0")]
+[assembly: AssemblyFileVersion("1.5.0")]
 
 // Common Language Specification (CLS) compliance generally refers to the claim that CLS rules and
 // restrictions are being followed.
