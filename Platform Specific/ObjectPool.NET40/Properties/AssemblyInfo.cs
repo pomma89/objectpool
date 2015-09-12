d5bdce868e0f0f727ee936973fafab53eef2dadd
Alessio Parma <alessio.parma@gmail.com>
2015-09-12 14:50:37 +0200

    using shared prj

--- Platform Specific/ObjectPool.NET40/Properties/AssemblyInfo.cs
+++ Platform Specific/ObjectPool.NET40/Properties/AssemblyInfo.cs
@@ -28,9 +28,6 @@ using System.Runtime.InteropServices;
 // you need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9fa5458c-89ab-4a37-9062-233a8fc52dfd")]
-
 // Version information for an assembly consists of the following four values:
 // 
 // Major Version Minor Version Build Number Revision
@@ -45,4 +42,4 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 
 // To allow simpler unit testing.
-[assembly: InternalsVisibleTo("UnitTests")]
+[assembly: InternalsVisibleTo("CodeProject.ObjectPool.UnitTests")]
