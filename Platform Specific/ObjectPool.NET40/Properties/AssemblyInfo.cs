0f6f3cc2944f4b4a4468ff072abc677fa77d84bf
Alessio Parma <alessio.parma@gmail.com>
2015-10-11 10:28:22 +0200

    bump to 1.9.0, signing

--- Platform Specific/ObjectPool.NET40/Properties/AssemblyInfo.cs
+++ Platform Specific/ObjectPool.NET40/Properties/AssemblyInfo.cs
@@ -34,12 +34,12 @@ using System.Runtime.InteropServices;
 // 
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.0")]
-[assembly: AssemblyFileVersion("1.8.0")]
+[assembly: AssemblyVersion("1.9.0")]
+[assembly: AssemblyFileVersion("1.9.0")]
 
 // Common Language Specification (CLS) compliance generally refers to the claim that CLS rules and
 // restrictions are being followed.
 [assembly: CLSCompliant(true)]
 
 // To allow simpler unit testing.
-[assembly: InternalsVisibleTo("CodeProject.ObjectPool.UnitTests")]
+[assembly: InternalsVisibleTo("CodeProject.ObjectPool.UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100efe22a8787b348e219d6b501b425d79f31502681d5a9f7d11030852e44e7ef2b29ddbc7dfcd6461fc3e67a6d7a186dea40535dad461679209a079a45440a99440bd292498b623e9a7fc5161c519c8d45b79fdfbd95a6e0ac5e211d5438d47b8635662d108448cb84bf1212983fab0010c68b41d5168bcbe5a59f9149974852c4")]
