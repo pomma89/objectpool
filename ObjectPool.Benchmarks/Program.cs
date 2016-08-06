032151eaaf3843b0443c8a472f393781c08c8007
Alessio Parma <alessio.parma@gmail.com>
2016-08-06 23:41:32 +0200

    benchmarks

--- ObjectPool.Benchmarks/Program.cs
+++ ObjectPool.Benchmarks/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ObjectPool.Benchmarks
+﻿namespace CodeProject.ObjectPool.Benchmarks
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
         }
     }
