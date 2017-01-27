d2fc06954976777b4a11f7e6af588251028b1c02
Alessio Parma <alessio.parma@gmail.com>
2017-01-27 17:48:52 +0100

    license to 2018

--- ObjectPool/Specialized/SpecializedPoolConstants.cs
+++ ObjectPool/Specialized/SpecializedPoolConstants.cs
@@ -4,7 +4,7 @@
 //
 // The MIT License (MIT)
 //
-// Copyright (c) 2013-2016 Alessio Parma <alessio.parma@gmail.com>
+// Copyright (c) 2013-2018 Alessio Parma <alessio.parma@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 // associated documentation files (the "Software"), to deal in the Software without restriction,
